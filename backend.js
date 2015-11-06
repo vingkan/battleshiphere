b1486269cd23d44ba64ce49da8d6d6668fdc94f7
Recursions <Nick@dhcp71.merunorth102.iit.edu>
2015-11-06 17:32:52 -0600

    Comments

--- backend.js
+++ backend.js
@@ -1,3 +1,4 @@
+console.log('LOADED BACKEND');
 //Dear source-code readers, pay no attention to these global variables
 var APP_ID = 'YtKfTS2Zo8ELUk63LzB0';
 var APP_CODE = 'SB1wcsUEGzLA3SRHJJ7CPw';
@@ -5,25 +6,22 @@ var APP_CODE = 'SB1wcsUEGzLA3SRHJJ7CPw';
 // Reference to the root of the Firebase database
 var database = new Firebase("https://battleshiphere.firebaseio.com/");
 
-$("#upload").click(function() {
+
+$("#upload").click(function() { // If the user clicks on the input with the id="upload"
 	var name = $("#nameInput").val();
 	var text = $("#messageInput").val();
 	if (name.length != 0 && text.length != 0) { // Don't upload if there's no information in the textboxes
 		console.log(name.length);
 		console.log(text.length);
 
-		database.push({name: name, text: text});
+		database.push({name: name, text: text}); // Push the data onto the database
 
-		database.on('child_added', function(update) {
+		database.on('child_added', function(update) { // Get an update once the database has new data
 			console.log("Callback function: New data has been added to the database");
 		});
-	}else {
+	}else { // warns user of their empty textbox
 		console.log("Please input values into the textbox");
 	}
 	
 });
 
-
-
-
-console.log('LOADED BACKEND');
