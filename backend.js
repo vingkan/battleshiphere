26dd619143818b5b7c552a75981366d87a8824a3
Recursions <Nick@dhcp55.merusouth115.iit.edu>
2015-11-06 03:28:09 -0600

    Allow user to push data onto Firebase

--- backend.js
+++ backend.js
@@ -2,4 +2,18 @@
 var APP_ID = 'YtKfTS2Zo8ELUk63LzB0';
 var APP_CODE = 'SB1wcsUEGzLA3SRHJJ7CPw';
 
+// Reference to the root of the Firebase database
+var database = new Firebase("https://brilliant-torch-6592.firebaseio.com/");
+
+$("#messageInput").keypress(function (e) {
+	if (e.keyCode == 13) {
+		var name = $("#nameInput").val();
+		var text = $("#messageInput").val();
+		database.push({name: name, text: text});
+		$("#messageInput").val("");
+	}
+
+
+});
+
 console.log('LOADED BACKEND');
