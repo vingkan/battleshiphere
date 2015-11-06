ee055505c9c3ff934f62089a3790a1a2a87dcabf
Vinesh Kannan <vingkan@gmail.com>
2015-11-06 02:40:41 -0600

    Added geolocation on data layer

--- data.js
+++ data.js
@@ -1,1 +1,24 @@
+/*--------------------------------------------*/
+/*---> GEOLOCATION <--------------------------*/
+/*--------------------------------------------*/
+
+var userPosition;
+
+function updatePosition(position){
+	userPosition = position.coords;
+}
+
+navigator.geolocation.getCurrentPosition(function(position){
+	updatePosition(position);
+});
+
+
+function getUserLat(){
+	return userPosition.latitude;
+}
+
+function getUserLon(){
+	return userPosition.longitude;
+}
+
 console.log('LOADED DATA');
