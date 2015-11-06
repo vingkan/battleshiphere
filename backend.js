7d84c4e68f98ee9fed2b2b88cca4ea133f37cbdf
Vinesh Kannan <vingkan@gmail.com>
2015-11-06 02:19:46 -0600

    Split into .js and .java teams

--- backend.js
+++ backend.js
@@ -1,5 +1,5 @@
 //Dear source-code readers, pay no attention to these global variables
-var APP_ID = YtKfTS2Zo8ELUk63LzB0;
-var APP_CODE = SB1wcsUEGzLA3SRHJJ7CPw;
+var APP_ID = 'YtKfTS2Zo8ELUk63LzB0';
+var APP_CODE = 'SB1wcsUEGzLA3SRHJJ7CPw';
 
 console.log('LOADED BACKEND');
