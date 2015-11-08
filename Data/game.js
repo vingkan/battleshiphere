a543adc1bd9cbf6b3aade254011af2443154bf86
Vinesh Kannan <vingkan@gmail.com>
2015-11-08 01:07:15 -0600

    Added Game.addTask(task)

---
+++ Data/game.js
@@ -0,0 +1,44 @@
+function Game(data){
+	this.id = data['id'];
+	this.name = data['name'];
+	this.players = [];
+	this.towers = [];
+	this.troops = [];
+	this.questions = [];
+	this.questionLength = 10;
+	this.spawnRate = 10;
+	this.mode = "lastmanstanding";
+	this.rounds = 0;
+	this.taskManager = {
+		taskList: [],
+		interval: 2000
+	}
+}
+
+Game.prototype.get = function(attribute){
+	return this[attribute];
+}
+
+Game.prototype.set = function(attribute, value){
+	this[attribute] = value;
+}
+
+Game.prototype.getObjectById = function(list, id){
+	var array = this[list];
+	console.log(array);
+	var response = null;
+	var size = array.length;
+	for(var i = 0; i < size; i++){
+		if(array[i] === id){
+			response = array[i];
+			break;
+		}
+	}
+	if(response != null){
+		return response;
+	}
+}
+
+Game.prototype.addTask = function(task){
+	this.taskManager.taskList.push(task);
+}
