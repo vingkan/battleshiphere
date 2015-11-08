3159432ce519f53ee32986422cf6821dd93354c2
Vinesh Kannan <vingkan@gmail.com>
2015-11-08 04:55:51 -0600

    Styled sidebars and menus

--- frontend.js
+++ frontend.js
@@ -1,1 +1,13 @@
+function toggleMenu(){
+	var presetTowers = $('#preset-towers');
+	if(presetTowers.hasClass('open')){
+		presetTowers.removeClass('open');
+		presetTowers.addClass('closed');
+	}
+	else{
+		presetTowers.addClass('open');
+		presetTowers.removeClass('closed');
+	}
+}
+
 console.log('LOADED FRONTEND');
